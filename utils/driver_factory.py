c8146325a1ef232874a80db736b1326507a9d059
Mauri4279 <ruizm4279@gmail.com>
2026-06-28 22:04:05 -0300

    feat: configurar CI/CD con GitHub Actions y ejecucion headless

--- utils/driver_factory.py
+++ utils/driver_factory.py
@@ -1,10 +1,17 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
 
 def get_driver():
-    """Inicializa y devuelve una instancia de Chrome WebDriver."""
-    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    chrome_options = Options()
+    # Las siguientes 3 líneas son VITALES para que funcione en GitHub Actions
+    chrome_options.add_argument("--headless") 
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
     
-    driver.maximize_window()
+    # max_window no hace nada en headless, pero sirve si lo corres en local
+    chrome_options.add_argument("--start-maximized") 
+    
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
     return driver
